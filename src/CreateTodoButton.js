13afb3d219c768ec34a45bf44f4794b622d2cedb
Juminstock <juminstock@gmail.com>
2023-02-22 13:03:26 -0500

    Added css files for each component

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CreateTodoButton.css"
 
 function CreateTodoButton(){
     return(
@@ -6,4 +7,4 @@ function CreateTodoButton(){
     );
 }
 
-export { CreateTodoButton };
+export {CreateTodoButton};
