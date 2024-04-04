684ede093ec7928f9cd547e1629f44b88e79e805
Carlos Rodríguez <juminstock@gmail.com>
2024-04-04 00:53:28 -0400

    Initial: Create next blank repository

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-96 font-bold">
+      <h1>Hello, Michael and Isaac!</h1>
+    </main>
+  );
+}
