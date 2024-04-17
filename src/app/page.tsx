0adf43aaa5c2373bf470711a7a6504a3d6399d41
Carlos Rodríguez <juminstock@gmail.com>
2024-04-17 17:45:05 -0400

    Delete all folders

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-96 font-bold">
-      <h1>Hello, Michael and Isaac!</h1>
-    </main>
-  );
-}
